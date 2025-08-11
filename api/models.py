17d8022d1f0dbfb6dd8e88aa9327037c7b4d850f
Mahesh Raskar <raskar.gwc@gmail.com>
2025-08-11 23:48:21 +0530

    updating new

--- api/models.py
+++ api/models.py
@@ -1,17 +1,18 @@
 from django.db import models
 from cloudinary_storage.storage import RawMediaCloudinaryStorage
-
 class Resource(models.Model):
     title = models.CharField(max_length=255)
-    description = models.TextField(blank=True, null=True)
-    document = models.ForeignKey('Document', on_delete=models.CASCADE,  null=False )
-    created_at = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(blank=True)
+    college = models.CharField(max_length=255)
+    branch = models.CharField(max_length=50)
+    resource_type = models.CharField(max_length=50)
+    file_url = models.URLField()
+    file_type = models.CharField(max_length=10)
+    size = models.IntegerField()  # Size in bytes
     upload_date = models.DateTimeField(auto_now_add=True)
-    branch = models.CharField(max_length=50, blank=True, null=False)  # Add if needed
 
     def __str__(self):
         return self.title
-
 class Document(models.Model):
     name = models.CharField(max_length=255)
     file = models.FileField(upload_to='documents/', storage=RawMediaCloudinaryStorage())
